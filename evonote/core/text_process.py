2e2a54a11ccb6b8e7333ecb5ce69afd1baff553f
Zijian Zhang <doomspec@outlook.com>
2023-07-31 16:49:07 +0800

    Add LaTeX pre-process

---
+++ evonote/core/text_process.py
@@ -0,0 +1,118 @@
+import re
+
+
+def latex_to_markdown(latex: str):
+    # replace \textit{xxx} with *xxx*
+    res = re.sub(r"\\textit{(.+?)}", r"*\1*", latex)
+    # replace \textbf{xxx} with **xxx**
+    res = re.sub(r"\\textbf{(.+?)}", r"**\1**", res)
+    # remove \cite{xxx}
+    res = re.sub(r"\\cite{.+?}", "[citations]", res)
+    # replace \footnote{xxx} with (xxx)
+    res = re.sub(r"\\footnote{(.+?)}", r"(\1)", res)
+    # replace \item with -
+    res = re.sub(r"\\item", "-", res)
+    # remove \begin{enumerate} and \end{enumerate}
+    res = re.sub(r"\\(begin|end){(enumerate|iterate)}", "\n", res)
+    # remove \label{xxx}
+    res = re.sub(r"\\label{.+?}", "", res)
+    return res
+
+
+def divide_text_into_section(section_level, tex):
+    searched = r"\\"+("sub"*section_level)+"section"+r"\*?{(.+?)}"
+    # find first section
+    m = re.search(searched, tex)
+    if m:
+        content = tex[:m.start()]
+        tex = tex[m.start():]
+    else:
+        return tex, []
+    sections = []
+    for m in re.finditer(searched, tex):
+        # get the section title
+        section_title = m.group(1)
+        # get the section content
+        section_content = tex[m.end():]
+        # find the end of the section content
+        end = re.search(searched, section_content)
+        if end:
+            section_content = section_content[:end.start()]
+        sections.append((section_title, section_content))
+
+    return content, sections
+
+
+def process_section_level(section_level, curr_level, doc):
+    if section_level == curr_level:
+        content, subsections = divide_text_into_section(section_level, doc["content"])
+        doc["content"] = content
+        for subsection in subsections:
+            doc["sections"].append({
+                "title": subsection[0],
+                "content": subsection[1].strip(),
+                "sections": []
+            })
+    else:
+        for i, section in enumerate(doc["sections"]):
+            process_section_level(section_level, curr_level+1, section)
+
+
+def process_latex_sections(tex: str, title: str):
+    abs = re.findall(r"\\begin{abstract}(.+?)\\end{abstract}", tex, re.DOTALL)[0].strip()
+    title = re.findall(r"\\title{(.+?)}", tex, re.DOTALL)[0].strip()
+    doc = {
+        "title": title,
+        "content": tex,
+        "sections": []
+    }
+    for i in range(0,4):
+        process_section_level(i, 0, doc)
+    return doc, {"abstract": abs, "title": title}
+
+def process_latex(latex: str):
+    """
+    Convert latex to markdown
+    :param latex:
+    :return:
+    """
+
+    # find \begin{abstract} and \end{abstract} and extract the text between
+    abs = re.findall(r"\\begin{abstract}(.+?)\\end{abstract}", latex, re.DOTALL)
+
+    # remove \begin{xxx}
+    res = re.sub(r"\\begin{.+?}", "", latex)
+    res = re.sub(r"\\end{.+?}", "", res)
+
+    return res
+
+def iter_sections(doc, root_path = ""):
+    if len(doc["content"]) > 0:
+        yield doc, root_path + "\\" + doc["title"]
+    for section in doc["sections"]:
+        yield from iter_sections(section, root_path = root_path + "\\" + doc["title"])
+
+
+def get_section_list(doc):
+    return list(iter_sections(doc))
+
+def to_paragraphs(text):
+    # separate by \n\n
+    paragraphs = text.split("\n\n")
+    # remove empty paragraphs
+    paragraphs = [p.strip() for p in paragraphs]
+    paragraphs = [p for p in paragraphs if len(p) > 0]
+    return paragraphs
+
+
+def process_latex_into_standard(tex: str):
+    tex = latex_to_markdown(tex)
+    doc, meta = process_latex_sections(tex)
+    #doc_dict = {"section_tree": doc, "meta": meta}
+    return doc, meta
+
+if __name__ == "__main__":
+    from pprint import pprint
+    from evonote.core.sample_paper import sample_paper
+    tex = sample_paper
+
