2f404e345c24b2921e50ed84db8d8fda3fc50a1a
Zijian Zhang <doomspec@outlook.com>
2023-08-25 14:20:59 -0400

    Add keyword_amp.py

---
+++ evonote/core/keyword_amp.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from evonote import EvolverInstance
+from evonote.data_type.chat import Chat
+from evonote.model.llm import complete_chat
+
+system_message = "You should output everything concisely as if you are a computer " \
+                 "program. "
+
+def keyword_amplify(keywords: List[str], n_limit=3, caller_path=None, use_cache=True):
+    """Return a list of keywords that are similar to the given keyword."""
+    if caller_path is None:
+        caller_path = EvolverInstance.get_caller_path()
+    cache_key = str(keywords)
+    cache = EvolverInstance.read_cache(cache_key, "keyword_amplify", caller_path, True)
+    if use_cache:
+        if cache.is_valid():
+            return cache._value
+    prompt = "You are a program that generates related keywords based on given keywords " \
+             "to help the search engine find related logs. "
+    prompt += "\nExisting keywords: " + "\n".join(keywords)
+    chat = Chat(user_message=prompt, system_message=system_message)
+    chat.add_user_message(f"Output at most {n_limit} related keywords. Separate each keyword by newline.")
+    res = complete_chat(chat)
+    res = res.split("\n")
+    res = [r.strip() for r in res]
+    res = [r for r in res if len(r) > 0]
+    cache.set_cache(res)
+    return res
+
+
+if __name__ == "__main__":
+    keywords = ["quantum computing", "quantum algorithm"]
+    print(keyword_amplify(keywords))
