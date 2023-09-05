4d5a9de5e171fe7a4c2d97bda39bdeb23acb4090
Zijian Zhang <doomspec@outlook.com>
2023-09-05 00:17:10 -0400

    chore: update requirements.txt & add openai key error

--- evonote/__init__.py
+++ evonote/__init__.py
@@ -2,3 +2,12 @@ import os
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+if openai.api_key is None:
+    print("You must set environment variable OPENAI_API_KEY before use")
+    raise Exception("OPENAI_API_KEY is not set")
+
+"""
+:module:
+This module is for knowledge base construction 
+"""
