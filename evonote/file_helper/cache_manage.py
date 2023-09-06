d6dca9168a94f701fd1f892369a1e6002c268c11
Zijian Zhang <doomspec@outlook.com>
2023-09-06 02:30:03 -0400

    feat: add unused cache garbage collection

--- evonote/file_helper/cache_manage.py
+++ evonote/file_helper/cache_manage.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import hashlib
 import json
 import os
-from typing import Dict, Optional, List
+from typing import Dict, Optional, List, Set
 
 from evonote.utils import get_main_path
 
@@ -16,6 +16,7 @@ class Cache:
     def __init__(self, value, hash: str, input: any, type: str,
                  meta: Optional[Dict] = None):
         self.value = value
+        # self.hash should be the same as get_hash(input, type)
         self.hash: str = hash
         self.input: any = input
         self.type: str = type
@@ -57,15 +58,25 @@ class CacheManager:
         if cache_path is None:
             cache_path = get_cache_file_path(get_main_path())
         self.cache_path = cache_path
-        # Map from the file path to the cache table
+        # Map from hash to cache
         self.cache_table: Dict[str, Cache] = self.load_cache_table()
         # List of pending cache
         self.pending_cache: List[Cache] = []
+        # List of active cache. Used for garbage collection
+        self.active_cache_hash: Set[str] = set()
 
     def save_all_cache_to_file(self):
         self.apply_cache_update()
         with open(self.cache_path, "w") as f:
             f.write(serialize_cache_table(self.cache_table))
+
+    def filter_unused_cache(self) -> int:
+        n_removed_cache = 0
+        for hash, cache in self.cache_table.items():
+            if hash not in self.active_cache_hash:
+                n_removed_cache += 1
+                del self.cache_table[hash]
+        return n_removed_cache
 
     def read_cache(self, input: any, type: str, create_cache=True) -> Cache | None:
         hash = get_hash(input, type)
@@ -77,7 +88,9 @@ class CacheManager:
                 return new_cache
             else:
                 return None
-        return cache_table[hash]
+        cache_hit = cache_table[hash]
+        self.active_cache_hash.add(hash)
+        return cache_hit
 
     def add_cache(self, cache: Cache):
         self.pending_cache.append(cache)
@@ -86,6 +99,7 @@ class CacheManager:
         remaining_cache = []
         for cache in self.pending_cache:
             if cache.is_valid():
+                self.active_cache_hash.add(cache.hash)
                 self.cache_table[cache.hash] = cache
             else:
                 remaining_cache.append(cache)
@@ -111,6 +125,13 @@ class CacheManager:
 
 
 def save_cache():
+    cache_manager.save_all_cache_to_file()
+
+
+def save_used_cache():
+    n_remove = cache_manager.filter_unused_cache()
+    if n_remove > 0:
+        print(f"Removed {n_remove} unused cache")
     cache_manager.save_all_cache_to_file()
 
 
--- playground/visualize_paper.py
+++ playground/visualize_paper.py
@@ -1,3 +1,4 @@
+from evonote.file_helper.cache_manage import save_used_cache
 from evonote.writer.build_from_sections import digest_all_descendants, \
     notebook_from_doc
 from evonote.data_cleaning.latex import process_latex_into_standard
@@ -12,3 +13,5 @@ paper_notebook = notebook_from_doc(doc, meta)
 digest_all_descendants(paper_notebook)
 
 paper_notebook.show_notebook_gui()
+
+save_used_cache()
