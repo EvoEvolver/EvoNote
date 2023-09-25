d153be0c02f29f5da0bb220d43391f7b43239e9e
Yuning <neuromancer43@gmail.com>
2023-09-25 12:04:14 +0200

    Refactor, rename latex.py as latex_converter

--- playground/visualize_paper.py
+++ playground/visualize_paper.py
@@ -1,7 +1,7 @@
 from evonote.file_helper.cache_manage import save_used_cache, save_cache
 from evonote.transform.build_from_sections import digest_all_descendants, \
     notebook_from_doc
-from evonote.data_cleaning.latex import process_latex_into_standard
+from evonote.data_cleaning.latex_converter import process_latex_into_standard
 
 
 tex = open("AI4Science.tex", "r", encoding="utf-8").read()
