3954db2df78e0756a0f5e5358ced75fb2a23c533
Zijian Zhang <doomspec@outlook.com>
2023-10-08 03:41:21 -0400

    chore: add github action for project tree

--- evonote/gui/notebook.py
+++ evonote/gui/notebook.py
@@ -15,26 +15,15 @@ h_en = Hyphenator('en_US')
 
 
 def draw_treemap(root: Note):
-    notebook = root.notebook
-    labels = []
-    parents = []
-    values = []
-    names = []
-    add_note_to_list(labels, parents, values, names, root, notebook)
 
-    line_width = 40
-    for i in range(len(values)):
-        if len(values[i].strip()) == 0:
-            continue
-        values[i] = hypenate_texts(values[i], line_width)
-        labels[i] = hypenate_texts(labels[i], line_width)
+    ids, labels, parents, texts = prepare_tree_parameters(root)
 
     fig = go.Figure(go.Treemap(
         labels=labels,
         parents=parents,
         # values=values,
-        ids=names,
-        text=values,
+        ids=ids,
+        text=texts,
         # text=values,
         root_color="lightgrey",
         # hoverinfo="label+text",
@@ -50,7 +39,36 @@ def draw_treemap(root: Note):
     fig.show()
 
 
-def add_note_to_list(labels, parents, values, names, note: Note, notebook: Notebook):
+def get_json_for_treemap(root: Note):
+    ids, labels, parents, texts = prepare_tree_parameters(root)
+    node_list = []
+    for i in range(len(ids)):
+        node_list.append({
+            "id": ids[i],
+            "label": labels[i],
+            "parent": parents[i],
+            "text": texts[i]
+        })
+    return node_list
+
+
+def prepare_tree_parameters(root):
+    notebook = root.notebook
+    labels = []
+    parents = []
+    texts = []
+    ids = []
+    add_note_to_list(labels, parents, texts, ids, root, notebook)
+    line_width = 40
+    for i in range(len(texts)):
+        if len(texts[i].strip()) == 0:
+            continue
+        texts[i] = hypenate_texts(texts[i], line_width)
+        labels[i] = hypenate_texts(labels[i], line_width)
+    return ids, labels, parents, texts
+
+
+def add_note_to_list(labels, parents, values, ids, note: Note, notebook: Notebook):
     i = 1
     children = notebook.get_children_dict(note)
     for key, child in children.items():
@@ -59,6 +77,6 @@ def add_note_to_list(labels, parents, values, names, note: Note, notebook: Noteb
         labels.append(label)
         parents.append("/".join(notepath[:-1]))
         values.append(child.content)
-        names.append("/".join(notepath))
-        add_note_to_list(labels, parents, values, names, child, notebook)
+        ids.append("/".join(notepath))
+        add_note_to_list(labels, parents, values, ids, child, notebook)
         i += 1
