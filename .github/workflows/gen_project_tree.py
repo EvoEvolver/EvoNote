f72d1c150252900c7b888c21f8eeb8f3ffdfed1e
Zijian Zhang <doomspec@outlook.com>
2023-10-09 21:33:00 -0400

    refactor: rename modules (I hope this is the last time)

--- .github/workflows/gen_project_tree.py
+++ .github/workflows/gen_project_tree.py
@@ -1,6 +1,6 @@
 import evonote, json
-from evonote.gui.notetree import get_json_for_treemap
-from evonote.transform.module_to_notetree import get_notetree_for_module
-notetree = get_notetree_for_module(evonote)
-tree = get_json_for_treemap(notetree.root)
+from evonote.gui.mindtree import get_json_for_treemap
+from evonote.transform.module_to_tree import get_tree_for_module
+tree = get_tree_for_module(evonote)
+tree_in_dict = get_json_for_treemap(tree.root)
 f = open('output/project_tree.json', 'w');json.dump(tree, f)
